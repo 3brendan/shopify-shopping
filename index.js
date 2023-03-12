140db8215ae7110b6e615c73321d7eeb01898e26
Brendan Le <bankingbrendan@gmail.com>
2023-03-11 19:17:05 -0500

    wip

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const { Client, IntentsBitField, messageLink} = require('discord.js');
 const fetch = require('node-fetch')
 const fs = require('fs');
+require('dotenv').config();
 
 var sitelist = fs.readFileSync('/Users/brendan/Desktop/shopify-shopping/sites.json');
 const data = JSON.parse(sitelist);
@@ -7,12 +9,25 @@ const data = JSON.parse(sitelist);
 var user_keywords = [''];
 const filtered_keywords = user_keywords.map(keyword => keyword.toLowerCase());
 
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ]
+});
+
+client.on('ready' , () =>{
+    console.log('online');   
+});
+
+var foundItems = [];
+
 for(let i = 0; i < data.websites.length; i++)
 {
-    //console.log(data.websites[i].website)
-    //     let url = `${perWeb}/products.json`
-
     const perWeb = data.websites[i].website
+
 
     let settings = { method: "Get" };
     let url = `${perWeb}/products.json`
@@ -25,9 +40,30 @@ for(let i = 0; i < data.websites.length; i++)
         {
             if((json.products[i].title).toLowerCase().includes(filtered_keywords))
             {
-                console.log(json.products[i].title);
+                foundItems.push(json.products[i].title);
+                //console.log(json.products[i].title + "\n");
+                console.log(foundItems);
             }
+
         }
     })
     .catch(() => console.log(`error parsing ` + perWeb));
 };
+
+client.on('messageCreate', (message) => {
+
+    if (message.author.bot)
+    {
+        return;
+    }
+    if (message.content == "ok")
+    {
+        // var foundItems = [];
+        // foundItems.push(json.products[i].title);
+        // message.reply(foundItems)
+        message.reply(foundItems.join("\n"));
+
+    }
+});
+
+client.login(process.env.TOKEN);
